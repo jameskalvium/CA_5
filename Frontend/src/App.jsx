a335c35576789767dd797207b218204990a5b1ec
unknown <james.r@kalvium.community>
2025-03-20 21:35:42 +0530

    correctedCode

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,11 +2,16 @@ import './App.css'
 
 import UserList from './components/UserList'
 import AddUser from './components/AddUser'
+import { Routes, Route } from 'react-router-dom'
 function App() {
 
   return (
-   <UserList />,
-   <AddUser />
+    <>
+    <Routes>
+      <Route path="/" element={<UserList />} />
+      <Route path="/add-user" element={<AddUser />} />
+    </Routes>
+    </>
   )
 }
 
